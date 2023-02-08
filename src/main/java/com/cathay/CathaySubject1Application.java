97d42fd822b69bebd06173220b718bcc87a2569a
WayneWen620 <WayneWen620@gmail.com>
2023-02-08 20:36:23 +0800

    init

---
+++ src/main/java/com/cathay/CathaySubject1Application.java
@@ -0,0 +1,13 @@
+package com.cathay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CathaySubject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CathaySubject1Application.class, args);
+	}
+
+}
