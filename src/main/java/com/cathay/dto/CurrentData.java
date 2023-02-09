42de603cf88cbaada901de156d2b280a23d55193
WayneWen620 <WayneWen620@gmail.com>
2023-02-10 02:12:25 +0800

    API初版功能
    (cherry picked from commit 6a21f79e65018307f08a2be69390c1eb9067ac10)

---
+++ src/main/java/com/cathay/dto/CurrentData.java
@@ -0,0 +1,16 @@
+package com.cathay.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrentData {
+	private String code;
+	private String name;
+	private float rate;
+}
